83ffe225e65bffa4fb21c608a99621a58b240521
ДимДим <1000rub@list.ru>
2025-03-04 13:32:21 +0300

    work040325

--- Tst/app.c
+++ Tst/app.c
@@ -1,43 +1,97 @@
-#include "stm32f10x.h"                  // Device header
+#include "stm32f10x.h"  // Подключите заголовочный файл для STM32F103
 
-void SPI_Transmit_DMA(uint8_t *data, uint16_t size) {
-    DMA1_Channel3->CNDTR = size; // Установите количество передаваемых данных
-    DMA1_Channel3->CMAR = (uint32_t)data; // Установите адрес данных
-    SPI1->CR2 |= SPI_CR2_TXDMAEN; // Включение DMA для передачи
-    DMA1_Channel3->CCR |= DMA_CCR3_EN; // Включение DMA-канала
+//void GPIO_Config(void) {
+//    // Включаем тактирование GPIOA
+//    RCC->APB2ENR |= RCC_APB2ENR_IOPAEN;
+//    
+//    // Настраиваем пины PA5, PA6, PA7 в альтернативную функцию
+//    GPIOA->CRL &= ~(GPIO_CRL_MODE5 | GPIO_CRL_MODE6 | GPIO_CRL_MODE7);
+//    GPIOA->CRL |= (GPIO_CRL_MODE5_1 | GPIO_CRL_CNF5_1); // PA5: выход, альт. функция, 2 MHz
+//    GPIOA->CRL |= (GPIO_CRL_MODE6_0 | GPIO_CRL_CNF6_0); // PA6: вход, флотирующий
+//    GPIOA->CRL |= (GPIO_CRL_MODE7_1 | GPIO_CRL_CNF7_1); // PA7: выход, альт. функция, 2 MHz
+//}
 
-    while (!(SPI1->SR & SPI_SR_TXE)); // Ожидание завершения передачи
-    SPI1->CR2 &= ~SPI_CR2_TXDMAEN; // Выключение DMA
-}
+//void SPI_Config(void) {
+//    // Включаем тактирование SPI1
+//    RCC->APB2ENR |= RCC_APB2ENR_SPI1EN;
 
-void Test_SPI1_DMA() {
-    uint8_t transmit_data[1] = {0x55}; // Данные для передачи
-    SPI_Transmit_DMA(transmit_data, 1); // Передача
-}
-	 
-  int main() 
-		{
-		char data;
-	 RCC->APB2ENR |= RCC_APB2ENR_IOPAEN;
-RCC->APB2ENR |= RCC_APB2ENR_SPI1EN;
-RCC->AHBENR |= RCC_AHBENR_DMA1EN;
-			
-	 GPIOA->CRL &= ~((0xF << (4 * 5)) | (0xF << (4 * 6)) | (0xF << (4 * 7)));
-GPIOA->CRL |= (0xB << (4 * 5)); // PA5 - SCK
-GPIOA->CRL |= (0x4 << (4 * 6)); // PA6 - MISO
-GPIOA->CRL |= (0xB << (4 * 7)); // PA7 - MOSI
-			
-	 SPI1->CR1 = 0; // Сброс настроек
-SPI1->CR1 |= SPI_CR1_MSTR | SPI_CR1_SPE | SPI_CR1_SSI | SPI_CR1_SSM; // Master, включить SPI
+//    // Сбрасываем настройки SPI
+//    SPI1->CR1 = 0;
+
+//    // Настраиваем параметры SPI: Мастер, частота делителя, режим CPOL/CPHA
+//    SPI1->CR1 |= SPI_CR1_MSTR       // Режим мастера
+//                | SPI_CR1_SPE        // Включить SPI
+//                | SPI_CR1_BR_1       // Частота SPI: Fpclk / 4 (можно настроить по необходимости)
+//                | SPI_CR1_CPOL       // Полярность (если необходимо)
+//                | SPI_CR1_CPHA;      // Фаза (если необходимо)
+
+//    // Включаем SPI
+//    SPI1->CR1 |= SPI_CR1_SPE;
+//}
+
+//uint8_t SPI_TransmitReceive(uint8_t data) {
+//    // Ждем, пока не будет готово устройство для передачи
+//    //while (!(SPI1->SR & SPI_SR_TXE)); // Проверка на наличие места для передачи
+//    SPI1->DR = data;                  // Отправка данных
+
+//    // Ждем, пока данные не будут приняты
+//    //while (!(SPI1->SR & SPI_SR_RXNE)); // Проверка на наличие данных для приема
+//    return SPI1->DR;                  // Прием данных и возвращение значения
+//}
+
+//int main(void) {
+//    GPIO_Config(); // Настройка GPIO для SPI
+//    SPI_Config();  // Настройка SPI
+
+//    // Пример передачи и приема данных
+//    uint8_t txData = 0xAA; // Данные для передачи
+//    uint8_t rxData;
+
+//    while (1) {
+//        rxData = SPI_TransmitReceive(txData); // Передача данных
+//        // Здесь добавьте обработку rxData по необходимости
+//    }
+//}
+
+
+ uint8_t SPI_TransmitReceive(uint8_t data) {
+       SPI1->DR = data; // Отправка данных
+       while (!(SPI1->SR & SPI_SR_RXNE)); // Ожидание окончания приема
+       return SPI1->DR; // Возврат принятого байта
+   }
+
+void Test_SPI1() {
+       uint8_t transmit_data = 0x55; // Данные для передачи
+       uint8_t received_data;
        
-			DMA1_Channel3->CCR &= ~DMA_CCR3_EN; // Отключите канал
-DMA1_Channel3->CNDTR = 1; // Количество данных
-DMA1_Channel3->CPAR = (uint32_t)&SPI1->DR; // Адрес регистра данных SPI
-DMA1_Channel3->CMAR = (uint32_t)&data; // Адрес памяти
-DMA1_Channel3->CCR |= DMA_CCR3_MINC | DMA_CCR3_DIR; // Инкремент адреса памяти, передача от памяти к периферии
-			
+       received_data = SPI_TransmitReceive(transmit_data); // Передача и прием
+       
+       // Обработка полученных данных, например:
+       if (received_data == transmit_data) {
+           // Успешная передача
+       } else {
+           // Ошибка передачи
+       }
+   }
+
+int main()
+	{
+		
+		RCC->APB2ENR |= RCC_APB2ENR_IOPAEN;
+   RCC->APB2ENR |= RCC_APB2ENR_SPI1EN;
+		
+		   GPIOA->CRL &= ~((0xF << (4 * 5)) | (0xF << (4 * 6)) | (0xF << (4 * 7)));
+   GPIOA->CRL |= (0xB << (4 * 5)); // PA5 - SCK
+   GPIOA->CRL |= (0x4 << (4 * 6)); // PA6 - MISO
+   GPIOA->CRL |= (0xB << (4 * 7)); // PA7 - MOSI
+		
+		SPI1->CR1 = 0; // Сброс настроек
+   SPI1->CR1 |= SPI_CR1_MSTR | SPI_CR1_SPE; // Master, Включить SPI
+		
+       // Настройка тактирования, GPIO и SPI1
+       
        while (1) {
-        Test_SPI1_DMA(); // Выполнение теста SPI с DMA
-        // Задержка или другая логика
-    }
+           Test_SPI1(); // Выполнение теста SPI
+           // Задержка или другая логика
+       }
    }
